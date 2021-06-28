a254641a4554cd0f85ac51838222d2e6f9bce038
meghalCB <86057039+meghalCB@users.noreply.github.com>
2021-06-28 18:00:20 +0530

    Add files via upload

---
+++ widgets/app_bar_title.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfire_samples/res/custom_colors.dart';
+
+class AppBarTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/fevi.png',
+          height: 20,
+        ),
+        SizedBox(width: 8),
+        Text(
+          'Vehi',
+          style: TextStyle(
+            color: CustomColors.firebaseYellow,
+            fontSize: 18,
+          ),
+        ),
+        Text(
+          'Brain',
+          style: TextStyle(
+            color: CustomColors.firebaseOrange,
+            fontSize: 18,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
